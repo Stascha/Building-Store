b5b10136a24e5d4d1dd95027eddd156d11f83260
Stascha <stasarasic@gmail.com>
2022-02-16 10:02:37 +0100

    Update Building_Store.java

--- Building_Store.java
+++ Building_Store.java
@@ -3,13 +3,11 @@ import java.util.*;
 public class Main
 {
     public static int solution( int K, int[][] A )
-    {
-        
+    {   
       int number_of_houses = 0;
       int rezult = 0;
       int distance_from_the_farthest_house;
-      int[][] Dist = new int[A.length][A[0].length];
-      
+     
        // Arrays of house coordinates            
       int[] xCordinates = new int[A.length * A[0].length];
       int[] yCordinates = new int[A.length * A[0].length]; 
@@ -19,8 +17,10 @@ public class Main
            if(A[x][y] == 1)
            {
                xCordinates[number_of_houses] = x;
-               yCordinates[number_of_houses] = y;
-               number_of_houses++;
+               yCordinates[number_of_houses++] = y;
+              
+               // -1 is placed in the cell in which the house is located
+               A[x][y] = -1;
            }
            
       xCordinates = Arrays.copyOf(xCordinates,number_of_houses);
@@ -29,26 +29,19 @@ public class Main
       for (int x = 0; x < A.length; x++)
         for (int y = 0; y < A[0].length; y++)
         {
-            // -1 is placed in the cell in which the house is located
-            for (int i = 0; i < number_of_houses; i++)
-               if( (x == xCordinates[i]) && (y == yCordinates[i])  )
-                  Dist[x][y] = -1;
-        
-            
             // Calculates the distance from the farthest house only for the cell in which the house is not located
-            if( Dist[x][y] != -1)
+            if( A[x][y] != -1)
                 for (int i = 0; i < number_of_houses; i++)
                 {
                     distance_from_the_farthest_house = Math.abs( xCordinates[i] - x ) + Math.abs( yCordinates[i] - y );
-                    Dist[x][y] = Dist[x][y] > distance_from_the_farthest_house ? Dist[x][y] : distance_from_the_farthest_house;
+                    A[x][y] = A[x][y] > distance_from_the_farthest_house ? A[x][y] : distance_from_the_farthest_house;
                 }
            
             // Counting the number of empty cells at a distance of at most K to every house.
-            if( Dist[x][y] <= K &&  Dist[x][y] != -1) 
+            if( A[x][y] <= K && A[x][y] != -1) 
                 rezult++;
             
         }
-                    
       
         return rezult;
     }
@@ -81,7 +74,6 @@ public class Main
         K = 1;
         int[][] C = new int[2][2];
         C[0][1] = 1;
-       
         
         s = solution( K ,  C );
        
@@ -91,7 +83,6 @@ public class Main
         K = 2;
         int[][] D = new int[10][10];
         D[0][1] = 1;
-       
         
         s = solution( K ,  D );
        
